d7d84986a6415e6517854975bd8e54cbf71fd378
logicoffee <riemann1618@gmail.com>
2021-04-22 22:13:44 +0900

    Fix port number

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import           Data.Maybe                           (fromMaybe)
 import           Data.String                          (fromString)
 import           Network.Wai.Handler.Warp             (run)
 import           Network.Wai.Middleware.RequestLogger (logStdout)
@@ -12,5 +11,4 @@ main :: IO ()
 main = do
     token  <- fromString <$> getEnv "CHANNEL_TOKEN"
     secret <- fromString <$> getEnv "CHANNEL_SECRET"
-    port   <- fmap read  <$> lookupEnv "PORT"
-    run (fromMaybe 8000 port) $ logStdout $ app token secret
+    run 8000 $ logStdout $ app token secret
